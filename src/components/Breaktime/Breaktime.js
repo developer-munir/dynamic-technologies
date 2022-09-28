ec57afe74f58b06a40c8d81bae868f5abbcdddca
developer-munir <devolopermunir@gmail.com>
2022-09-29 00:18:39 +0600

    responsive details menu

--- src/components/Breaktime/Breaktime.js
+++ src/components/Breaktime/Breaktime.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Breaktime = () => {
     return (
       <div>
-        <div className="flex items-center justify-between border p-2 my-4">
-          <h1>ex</h1>
-          <h1>ex</h1>
+        <div className="flex items-center justify-between border p-2 my-4 rounded-lg">
+          <h1>Break time</h1>
+          <h1>00 seconds</h1>
         </div>
       </div>
     );
--- src/components/Exdetails/Exdetails.js
+++ src/components/Exdetails/Exdetails.js
@@ -4,9 +4,9 @@ const Exdetails = () => {
     return (
       <div>
         <h1 className="text-2xl font-semibold">Exercise Details</h1>
-        <div className="flex items-center justify-between border p-2 my-4">
-          <h1>ex</h1>
-          <h1>ex</h1>
+        <div className="flex items-center justify-between border p-2 my-4 rounded-lg">
+          <h1>Exercise time</h1>
+          <h1>00 seconds</h1>
         </div>
       </div>
     );
